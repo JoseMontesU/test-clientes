1b10b97437ac905017aa2cf941a728025361a049
JoseMontesU <jmontesu@bcds.net>
2025-06-21 01:00:41 -0500

    se agrego el script sql

---
+++ clientes.sql
@@ -0,0 +1,22 @@
+CREATE TABLE cliente(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    documento ENUM('DNI', 'CARNET DE EXTRANJERIA') NOT NULL,
+    numero_documento VARCHAR(20) NOT NULL UNIQUE,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    lugar_residencia VARCHAR(100) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
+    genero ENUM('M', 'F') NOT NULL,
+    bono_bienvenida DECIMAL(10, 2) DEFAULT 0.00,
+    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE parametros_globales (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL UNIQUE,
+    valor boolean NOT NULL,
+    descripcion TEXT,
+    fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
