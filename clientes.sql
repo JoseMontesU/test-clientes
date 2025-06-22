c74dda5e5ceeaf22a8bb30759d5c8f26a78e29cd
AyrtonTC-06 <elmerayrtontisnadocharana@gmail.com>
2025-06-21 22:03:03 -0500

    modificacion de script-clientes

--- clientes.sql
+++ clientes.sql
@@ -1,18 +1,21 @@
-CREATE TABLE cliente(
+CREATE TABLE cliente IF NOT EXISTS clientes (
+   CREATE TABLE cliente (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    documento ENUM('DNI', 'CARNET DE EXTRANJERIA') NOT NULL,
+    tipo_documento ENUM('DNI', 'Carnet de extranjería') NOT NULL,
     numero_documento VARCHAR(20) NOT NULL UNIQUE,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
     fecha_nacimiento DATE NOT NULL,
     lugar_residencia VARCHAR(100) NOT NULL,
     telefono VARCHAR(15) NOT NULL,
+    correo VARCHAR(150) NOT NULL UNIQUE,
     genero ENUM('M', 'F') NOT NULL,
-    bono_bienvenida DECIMAL(10, 2) DEFAULT 0.00,
+    bono_bienvenida DECIMAL(10,2) DEFAULT 0.00,
     fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE parametros_globales (
+);
+CREATE TABLE parametros_globales IF NOT EXISTS parametros_globales (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL UNIQUE,
     valor boolean NOT NULL,
